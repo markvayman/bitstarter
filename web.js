b7bf959ab09c31e524bb47c52461b752ce3a7cb5
Mark Vayman <markvayman@hotmail.com>
2013-08-01 22:01:03 -0700

    deleted images from root

--- web.js
+++ web.js
@@ -12,5 +12,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on  " + port);
 });
